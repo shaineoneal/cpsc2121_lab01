efcaf1a1e5b2fbc2ea302b77295db000cfef1048
Shaine O'Neal <shaineoneal@gmail.com>
2022-09-07 10:27:28 -0400

    list.h finished

--- List.h
+++ List.h
@@ -58,11 +58,21 @@ class List{
 template <class T>
 List<T>::List(){
   start = nullptr;
+  mySize = 0;
 }
 
 //Destroy all nodes in this list to prevent memory leaks
 template <class T>
 List<T>::~List(){
+  Node<T> * iterator;
+  iterator = start;
+
+  for(int count = 0; count<mySize; count ++) {
+    Node<T> * del;
+    del = iterator;
+    iterator = del->next;
+    delete del;
+  }
 }
 
 //Return the size of this list
@@ -75,74 +85,142 @@ int List<T>::size(){
 //Otherwise, return false
 template <class T>
 bool List<T>::empty(){
-  if (start == NULL)
+  if (mySize == 0)
   {
-    return TRUE;
+    return true;
   }
-//make sure u aren't being an idiot
-  else {return FALSE}
+  else {return false;}
 }
 
 //Create a new node with value, and insert that new node
 //into this list at start
 template <class T>
 void List<T>::insertStart(T value){
-  Node(value);
-
-  next->
+  Node<T> * temp = new Node<T>(value);
+  temp->next = start;
+  start = temp;
 }
 
 //Create a new node with value, and insert that new node
 //into this list at end
 template <class T>
 void List<T>::insertEnd(T value){
-  start->value = value;
-  start->next = NULL;
+  Node<T> * temp = new Node<T>(value);
+  Node<T> * iterator = start;
+  
+  while(iterator->next != NULL) {
+    iterator = iterator->next;
+  }
+
+  iterator->next = temp;
 }
 
 //Create a new node with value <value>, and insert that new node at position j
 template <class T>
 void List<T>::insertAt(T value, int j){
+  Node<T> * temp = new Node<T>(value);
+  Node<T> * iterator = start;
+  int position = 0;
+
+  while(position != j) {
+    iterator = iterator->next;
+    position++;
+  }
+
+  temp->next = iterator->next;
+  iterator->next = temp;
 }
 
 //Remove node at start
 //Make no other changes to list
 template <class T>
 void List<T>::removeStart(){
+  Node<T> * iterator = start;
+  start = iterator->next;
+  delete iterator;
 }
 
 //Remove node at end
 //Make no other changes to list
 template <class T>
 void List<T>::removeEnd(){
+  Node<T> * iterator = start;
+
+  while(iterator->next != NULL) {
+    iterator = iterator-> next;
+  }
+
+  delete iterator;
 }
 
 //Remove node at position j
 //Make no other changes to list
 template <class T>
 void List<T>::removeAt(int j){
+  int position = 0;
+  Node<T> * iterator = start;
+
+  while(position != j) {
+    iterator = iterator->next;
+    position++;
+  }
 }
 
 //Return the value of the first node in the Linked List,
 //If no first node, return the default constructed value: T()
 template <class T>
 T List<T>::getFirst(){
+  if(mySize == 0) {return T();}
+
+  return start->value;
 }
 
 //Return the value of the last node in the Linked List,
 //If no first node, return the default constructed value: T()
 template <class T>
 T List<T>::getLast(){
+  if(mySize == 0) {return T();}
+
+  Node<T> * iterator = start;
+
+  while(iterator->next != NULL) {
+    iterator = iterator->next;
+  }
+
+  return iterator->value;
 }
 
 //Return the value of the node at position j in the Linked List,
 //If no first node, return the default constructed value: T()
 template <class T>
 T List<T>::getAt(int j){
+  if(mySize == 0) {return T();}
+
+  Node<T> * iterator = start;
+
+  int position = 0;
+
+  while (position != j) {
+    iterator = iterator->next;
+    position++;
+  }
+
+  return iterator->value;
 }
 
 //Return the position of the (first) node whose value is equal to the key
 //Otherwise, return -1
 template <class T>
 int List<T>::find(T key){
+  Node<T> * iterator = start;
+  int position = 0;
+
+  while(iterator->data != key && position<mySize) {
+    iterator = iterator->next;
+    position++;
+  }
+
+  if(iterator->value != key) {return -1;}
+
+  return position;
 }
